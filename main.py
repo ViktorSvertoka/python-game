5100973dbb45c2744d227310bb43721cca0e9a8b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-24 00:02:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ is_working = True
 
 ball = pygame.Surface((20, 20))
 ball.fill((255, 255, 255))
+bal_rect = ball.get_rect()
+ball_speed = [1, 1]
 
 while is_working:
     for event in pygame.event.get():
         if event.type == QUIT:
             is_working = False
 
-
-    main_surface.blit(ball, (100, 100))
+    bal_rect = bal_rect.move(ball_speed)
+    main_surface.blit(ball, bal_rect)
 
     # main_surface.fill((255, 155, 200))
-
+ 
     pygame.display.flip()
